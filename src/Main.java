818af86663fcd69811a3bbff011d8f26f84317f7
Sebastian Borquez <sebastian.borquez.l@gmail.com>
2020-09-12 15:09:46 -0300

    Commit del Main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
+        Estudiante e1 = new Estudiante("Jaime", 20);
+        Academico l1 = new Academico("Patricio", 50, "Ciencia de lo absurdo");
 
     }
 }
